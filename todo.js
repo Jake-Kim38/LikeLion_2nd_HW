8a2952ded1e023c1181c92d199ee028ab3f5ff7f
Jungmin Kim <kjm25034841@gmail.com>
2023-04-05 13:47:01 +0900

    todo.js 완료

--- todo.js
+++ todo.js
@@ -33,55 +33,85 @@ let id = 0;
  * @param {'create' | 'read' | 'update' | 'delete'} command
  * @param {{ id: number, title: string, text: string } | null} value
  * */
-const todoControl = (command, value) => {};
+const todoControl = (command, value) => {
+  switch (command) {
+    case "create":
+      createTodo(value);
+      break;
+    case "read":
+      readTodo(value);
+      break;
+    case "update":
+      updateTodo(value);
+      break;
+    case "delete":
+      deleteTodo(value);
+      break;
+    default:
+      console.log("command error!");
+      break;
+  }
+};
 
 // 보너스: newTodo를 비구조화 할당하여 todos에 추가하세요.
 /** @param {{ id: number, title: string, text: string }} newTodo */
 const createTodo = (newTodo) => {
   // todos에 새로운 todo를 삽입
+  todos.push(newTodo);
 };
 
 // 출력 형식: 'id: 0, ttile: 타이틀, text: 텍스트'
 // 보너스: 리터럴 문자를 활용해 출력하세요.
 const readTodo = () => {
   // todos를 하나씩 출력 형식에 맞춰 출력
+  for (const todo of todos) {
+    console.log(`id: ${todo.id}, title: ${todo.title}, text: ${todo.text}`);
+  }
 };
 
 // 에러처리: 만약 업데이트하려는 todo를 찾을 수 없는 경우 'id = (n)에 맞는 todo를 찾을 수 없습니다.' 를 출력하세요.
 // 보너스: if문을 쓰지 말고 업데이트 하세요. 단, 에러 처리에는 if문을 써도 됩니다. (힌트: 논리연산자)
 /** @param {{ id: number, title: string, text: string }} targetTodo */
 const updateTodo = (targetTodo) => {
-  // if (업데이트 하고자 하는 todo를 찾을 수 없다면) {
-  //   return 에러 메시지를 출력하세요
-  // }
-  // 여기에 업데이트 로직을 작성하세요
+  // 찾고자 하는 todo와 기존 todo의 id를 비교하여 filter, 없다면 빈칸을 반환하므로 length는 0이 될 것임.
+  const exists = todos.filter((todo) => todo.id === targetTodo.id);
+  if (exists.length === 0) {
+    return console.log(
+      `id = (${targetTodo.id})에 맞는 todo를 찾을 수 없습니다.`
+    );
+  }
+  // 찾고자 하는 todo와 기존 todo의 id가 같다면 기존 todo를 targetTodo로 업데이트, 아니면 그대로.
+  todos = todos.map((todo) => (todo.id === targetTodo.id ? targetTodo : todo));
 };
 
 // 에러처리: 만약 업데이트하려는 todo를 찾을 수 없는 경우 'id = n에 맞는 todo를 찾을 수 없습니다.' 를 출력하세요.
 // 보너스: filter 함수를 사용해 구현해보세요
 /** @param {number} id */
 const deleteTodo = (id) => {
-  // if (해당하는 todo id가 todo 데이터에 없다면) {
-  //   return console.log(여기에 에러 메시지를 쓰세요);
-  // }
-  // 여기에 삭제 로직을 작성하세요
+  // 찾고자 하는 todo와 기존 todo의 id를 비교하여 filter, 없다면 빈칸을 반환하므로 length는 0이 될 것임.
+  const exists = todos.filter((todo) => todo.id === id);
+  if (exists.length === 0) {
+    return console.log(`id = (${id})에 맞는 todo를 찾을 수 없습니다.`);
+  }
+  // deleteTodo의 인자로 넘어온 id를 제외한 나머지로 todo 리스트를 갱신
+  todos = todos.filter((todo) => todo.id !== id);
 };
 
 /** @info 테스트케이스, 이하 코드는 조작하지 마세요. '통과!!' 가 출력되어야합니다 */
 const test = () => {
-  todoControl('bug!!!');
+  todoControl("bug!!!");
   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((idx) =>
-    todoControl('create', {
+    todoControl("create", {
       id: id++,
       title: `title${idx}`,
       text: `text${idx}`,
     })
   );
-  [1, 2, 10].map((id) => todoControl('delete', id));
+  [1, 2, 10].map((id) => todoControl("delete", id));
   [3, 4, 100].map((id) =>
-    todoControl('update', { id: id, title: `타이틀${id}`, text: `텍스트${id}` })
+    todoControl("update", { id: id, title: `타이틀${id}`, text: `텍스트${id}` })
   );
-  todoControl('read');
+  todoControl("read");
 
   const testValue = [0, 3, 4, 5, 6, 7, 8, 9].map((id) => {
     if (id === 3 || id === 4) {
@@ -93,8 +123,8 @@ const test = () => {
   // true가 출력되어야합니다.
   console.log(
     JSON.stringify(testValue) === JSON.stringify(todos)
-      ? '통과!!'
-      : '실패!! 다시 풀어보세요'
+      ? "통과!!"
+      : "실패!! 다시 풀어보세요"
   );
 };
 
